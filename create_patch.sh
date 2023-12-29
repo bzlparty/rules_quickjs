39df55710badc7a6a5b53e673ab189a02fd7f096
Marc Redemske <code@mgred.dev>
2023-12-29 17:10:50 +0100

    chore: cleanup

--- create_patch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-
-for f in $(find ./vendor -name "*.bazel"); do
-  echo $f
-done;
-
-# git diff --no-index /dev/null vendor/quickjs.BUILD.bazel | tail -n +4 | sed "s|b/vendor/quickjs.||" > vendor/quickjs_build_dot_bazel.patch
