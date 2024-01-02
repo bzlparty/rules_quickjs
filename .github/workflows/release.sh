4bf08ad74bda18e23cf192d92aa9fb48d4e8cc35
Marc Redemske <code@mgred.dev>
2024-01-02 10:15:37 +0100

    chore: init

---
+++ .github/workflows/release.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+host=$(uname -s)
+NAME=project_name
+TAG=${GITHUB_REF_NAME}
+VERSION=${TAG:1}
+PREFIX="${NAME}-${VERSION}"
+RULES_ARCHIVE="${NAME}-${TAG}.tar.gz"
+
+echo -n "build: Create Rules Archive"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip >$RULES_ARCHIVE
+RULES_SHA=$(shasum -a 256 $RULES_ARCHIVE | awk '{print $1}')
+echo " ... done ($RULES_ARCHIVE: $RULES_SHA)"
+
+echo -n "build: Creaet Release Notes"
+cat > release_notes.md <<EOF
+
+## Installation
+
+> [!IMPORTANT]  
+> Installation is only supported via Bzlmod!
+
+### Install from Git
+
+\`\`\`starlark
+bazel_dep(name = "bzlparty_project_name")
+
+git_override(
+    module_name = "bzlparty_project_name",
+    remote = "git@github.com:bzlparty/project_name.git",
+    commit = "${GITHUB_SHA}",
+)
+\`\`\`
+
+### Install from Archive
+
+\`\`\`starlark
+bazel_dep(name = "bzlparty_project_name")
+
+archive_override(
+    module_name = "bzlparty_project_name",
+    urls = "https://github.com/bzlparty/project_name/releases/download/${TAG}/${RULES_ARCHIVE}",
+    strip_prefix = "${PREFIX}",
+    integrity = "sha256-${RULES_SHA}",
+)
+\`\`\`
+
+## Checksums
+
+**${RULES_ARCHIVE}** ${RULES_SHA}
+
+EOF
+
+echo " ... done"
---
+++ README.md
@@ -0,0 +1,25 @@
+# Bazel Rules for target_name
+
+[![Test](https://github.com/bzlparty/project_name/actions/workflows/test.yaml/badge.svg?branch=main&event=push)](https://github.com/bzlparty/project_name/actions/workflows/test.yaml)
+
+## Installation
+
+See install instructions on the [release page](https://github.com/bzlparty/project_name/releases).
+
+## Usage
+
+```
+@TODO
+```
+
+## Development
+
+Install git hooks:
+
+```bash
+pre-commit install
+```
+
+## License
+
+[GNU GPL 3.0](/LICENSE)
