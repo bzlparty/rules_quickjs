78bc97f31fb32bebc8f0c452b0b6d04f2180d38d
Marc Redemske <code@mgred.dev>
2024-01-05 12:15:02 +0100

    chore: add bcr install instructions

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -20,6 +20,15 @@ cat > release_notes.md <<EOF
 > [!IMPORTANT]  
 > Installation is only supported via Bzlmod!
 
+Choose from the options below and put as dependency in your `MODULE.bazel`.
+
+### Install from BCR
+
+\`\`\`starlark
+bazel_dep(name = "bzlparty_project_name", version = "${VERSION}")
+\`\`\`
+
+
 ### Install from Git
 
 \`\`\`starlark
